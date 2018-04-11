455ae7f31a9145c78cedb02214b7096cefaa521d
Paweł Sosnowiec <pawel.sosnowiec@wp.pl>
2018-04-11 15:34:23 +0200

    first

---
+++ Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Entity;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        AWE dbContext;
+        public Form1()
+        {
+            InitializeComponent();
+            dbContext = new AWE();
+            dbContext.Product.Load();
+            productBindingSource.DataSource = dbContext.Product.Local.ToBindingList();
+        }
+    }
+}
